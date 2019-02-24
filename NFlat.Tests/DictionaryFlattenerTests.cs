cb308bc5fddb42a76d86369414acfdd6034ea2c0
lionhunter <lionhunter3k@yahoo.com>
2019-02-24 17:52:29 +0200

    first commit

---
+++ NFlat.Tests/DictionaryFlattenerTests.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace NFlat.Tests
+{
+    [TestFixture]
+    public class DictionaryFlattenerTests
+    {
+        [Test]
+        public void Simple_Objects_Are_Same()
+        {
+            var flatObject = new Dictionary<string, string>()
+            {
+                { "Username", "John" },
+                { "Email", "something@something.com" }
+            };
+            var unflattenedObject = new DictionaryFlattener().Unflatten(flatObject);
+            Assert.IsTrue(unflattenedObject.ContainsKey("Username"));
+            Assert.AreEqual("John", unflattenedObject["Username"]);
+            Assert.IsTrue(unflattenedObject.ContainsKey("Email"));
+            Assert.AreEqual("something@something.com", unflattenedObject["Email"]);
+        }
+
+        [Test]
+        public void First_Level_Nested_Objects_Should_Work()
+        {
+            var flatObject = new Dictionary<string, string>()
+            {
+                { "Address_Street", "Victory Street" },
+                { "Address_PhoneNumber", "321321423" },
+                { "Username", "John" },
+                { "Email", "something@something.com" },
+            };
+            var unflattenedObject = new DictionaryFlattener().Unflatten(flatObject);
+            Assert.IsTrue(unflattenedObject.ContainsKey("Username"));
+            Assert.AreEqual("John", unflattenedObject["Username"]);
+            Assert.IsTrue(unflattenedObject.ContainsKey("Email"));
+            Assert.AreEqual("something@something.com", unflattenedObject["Email"]);
+            Assert.IsTrue(unflattenedObject.ContainsKey("Address"));
+            var address = unflattenedObject["Address"] as Dictionary<string, object>;
+            Assert.IsNotNull(address);
+            Assert.IsTrue(address.ContainsKey("Street"));
+            Assert.AreEqual("Victory Street", address["Street"]);
+            Assert.IsTrue(address.ContainsKey("PhoneNumber"));
+            Assert.AreEqual("321321423", address["PhoneNumber"]);
+        }
+
+        [Test]
+        public void Second_Level_Nested_Objects_Should_Work()
+        {
+            var flatObject = new Dictionary<string, string>()
+            {
+                { "Address_Country_Symbol", "ro" },
+                { "Address_Country_Name", "Romania" },
+                { "Address_Street", "Victory Street" },
+                { "Address_PhoneNumber", "321321423" },
+                { "Username", "John" },
+                { "Email", "something@something.com" },
+            };
+            var unflattenedObject = new DictionaryFlattener().Unflatten(flatObject);
+            Assert.IsTrue(unflattenedObject.ContainsKey("Username"));
+            Assert.AreEqual("John", unflattenedObject["Username"]);
+            Assert.IsTrue(unflattenedObject.ContainsKey("Email"));
+            Assert.AreEqual("something@something.com", unflattenedObject["Email"]);
+            Assert.IsTrue(unflattenedObject.ContainsKey("Address"));
+            var address = unflattenedObject["Address"] as Dictionary<string, object>;
+            Assert.IsNotNull(address);
+            Assert.IsTrue(address.ContainsKey("Street"));
+            Assert.AreEqual("Victory Street", address["Street"]);
+            Assert.IsTrue(address.ContainsKey("PhoneNumber"));
+            Assert.AreEqual("321321423", address["PhoneNumber"]);
+            Assert.IsTrue(address.ContainsKey("Country"));
+            var country = address["Country"] as Dictionary<string, object>;
+            Assert.IsNotNull(country);
+            Assert.IsTrue(country.ContainsKey("Symbol"));
+            Assert.AreEqual("ro", country["Symbol"]);
+            Assert.IsTrue(country.ContainsKey("Name"));
+            Assert.AreEqual("Romania", country["Name"]);
+        }
+
+    }
+}
---
+++ NFlat/DictionaryFlattener.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NFlat
+{
+    public class DictionaryFlattener
+    {
+        public Dictionary<string, object> Unflatten(Dictionary<string, string> data)
+        {
+            var result = new Dictionary<string, object>();
+            (Dictionary<string, object> @object, List<object> list) cur = default;
+            var idx = 0;
+            string prop = null;
+            string temp = null;
+            foreach(var p in data.Keys)
+            {
+                cur.@object = result;
+                prop = string.Empty;
+                var last = 0;
+                do
+                {
+                    idx = p.IndexOf("_", last);
+                    temp = idx != -1 ? p.Substring(last, idx - last) : p.Substring(last);
+                    if(cur.@object != null)
+                    {
+                        if(!cur.@object.ContainsKey(prop))
+                        {
+                            if (int.TryParse(temp, out var _))
+                            {
+                                cur.@object.Add(prop, new List<object>());
+                            }
+                            else
+                            {
+                                cur.@object.Add(prop, new Dictionary<string, object>());
+                            }
+                        }
+                        if (cur.@object[prop] is Dictionary<string, object>)
+                        {
+                            cur.@object = cur.@object[prop] as Dictionary<string, object>;
+                            cur.list = null;
+                        }
+                        else
+                        {
+                            cur.@object = null;
+                            cur.list = cur.@object[prop] as List<object>;
+                        }
+                    }
+                    else
+                    {
+                        var index = int.Parse(prop);
+                        if (int.TryParse(temp, out var _))
+                        {
+                            cur.list.Add(new List<Dictionary<string, object>>());
+                        }
+                        else
+                        {
+                            cur.list.Add(new Dictionary<string, object>());
+                        }
+                        if (cur.list[index] is Dictionary<string, object>)
+                        {
+                            cur.@object = cur.list[index] as Dictionary<string, object>;
+                            cur.list = null;
+                        }
+                        else
+                        {
+                            cur.@object = null;
+                            cur.list = cur.list[index] as List<object>;
+                        }
+                    }
+                    prop = temp;
+                    last = idx + 1;
+                } while (idx >= 0);
+                cur.@object?.Add(prop, data[p]);
+                cur.list?.Add(data[p]);
+            }
+            return result[string.Empty] as Dictionary<string, object>;
+        }
+    }
+}
