5cfff92d5e0691603600a15b29efd31b5749badc
Lionhunter3k <lionhunter@gmail.com>
2019-02-25 02:35:18 +0200

    support lists as well

--- NFlat.Tests/ObjectFlattenerTests.cs
+++ NFlat.Tests/ObjectFlattenerTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace NFlat.Tests
 {
@@ -12,6 +13,8 @@ namespace NFlat.Tests
         public string Email { get; set; }
 
         public Address Address { get; set; }
+
+        public List<Country> Countries { get; set; }
     }
 
     public class Address
@@ -21,6 +24,8 @@ namespace NFlat.Tests
         public int PhoneNumber { get; set; }
 
         public Country Country { get; set; }
+
+        public List<int> Ids { get; set; }
     }
 
     public class Country
@@ -84,7 +89,7 @@ namespace NFlat.Tests
                 { "Address_Street", "Victory Street" },
                 { "Address_PhoneNumber", "321321423" },
                 { "Username", "John" },
-                { "Email", "something@something.com" },
+                { "Email", "something@something.com" }
             };
             var unflattenedObject = new ObjectFlattener<User, string>()
                .MapProperty(nameof(User.Username), new StringPropertyMap<User>((u, v) => u.Username = v))
@@ -106,6 +111,64 @@ namespace NFlat.Tests
             Assert.IsNotNull(country);
             Assert.AreEqual("ro", country.Symbol);
             Assert.AreEqual("Romania", country.Name);
+        }
+
+        [Test]
+        public void Nested_Lists_Should_Work()
+        {
+            var flatObject = new Dictionary<string, string>()
+            {
+                { "Address_Country_Symbol", "ro" },
+                { "Address_Country_Name", "Romania" },
+                { "Address_Street", "Victory Street" },
+                { "Address_PhoneNumber", "321321423" },
+                { "Address_Ids_0", "1" },
+                { "Address_Ids_1", "2" },
+                { "Username", "John" },
+                { "Email", "something@something.com" },
+                { "Countries_0_Symbol", "en" },
+                { "Countries_0_Name", "UK" },
+                { "Countries_1_Symbol", "us" },
+                { "Countries_1_Name", "USA" },
+            };
+            var unflattenedObject = new ObjectFlattener<User, string>()
+               .MapProperty(nameof(User.Username), new StringPropertyMap<User>((u, v) => u.Username = v))
+               .MapProperty(nameof(User.Email), new StringPropertyMap<User>((u, v) => u.Email = v))
+               .MapNested(nameof(User.Address), new GenericConstructorMap<User>(u => u.Address = new Address(), u => u.Address))
+               .MapProperty("Address_Street", new StringPropertyMap<Address>((u, v) => u.Street = v))
+               .MapProperty("Address_PhoneNumber", new Int32PropertyMap<Address>((u, v) => u.PhoneNumber = v))
+               .MapNested("Address_Country", new GenericConstructorMap<Address>(u => u.Country = new Country(), u => u.Country))
+               .MapProperty("Address_Country_Symbol", new StringPropertyMap<Country>((u, v) => u.Symbol = v))
+               .MapProperty("Address_Country_Name", new StringPropertyMap<Country>((u, v) => u.Name = v))
+               .MapNested("Address_Ids", new GenericConstructorMap<Address>(u => u.Ids = new List<int>(), u => u.Ids))
+               .MapProperty("Address_Ids_*", new Int32PropertyMap<List<int>>((u, v) => u.Add(v)))
+               .MapNested("Countries", new GenericConstructorMap<User>(u => u.Countries = new List<Country>(), u => u.Countries))
+               .MapNested("Countries_*", new GenericConstructorMap<List<Country>>((u) => u.Add(new Country()), u => u.Last()))
+               .MapProperty("Countries_*_Symbol", new StringPropertyMap<Country>((u, v) => u.Symbol = v))
+               .MapProperty("Countries_*_Name", new StringPropertyMap<Country>((u, v) => u.Name = v))
+               .Unflatten(flatObject);
+            Assert.AreEqual("John", unflattenedObject.Username);
+            Assert.AreEqual("something@something.com", unflattenedObject.Email);
+            var address = unflattenedObject.Address;
+            Assert.IsNotNull(address);
+            Assert.AreEqual("Victory Street", address.Street);
+            Assert.AreEqual(321321423, address.PhoneNumber);
+            var country = address.Country;
+            Assert.IsNotNull(country);
+            Assert.AreEqual("ro", country.Symbol);
+            Assert.AreEqual("Romania", country.Name);
+            var ids = address.Ids;
+            Assert.IsNotNull(ids);
+            Assert.AreEqual(2, ids.Count);
+            Assert.AreEqual(1, ids[0]);
+            Assert.AreEqual(2, ids[1]);
+            var countries = unflattenedObject.Countries;
+            Assert.IsNotNull(countries);
+            Assert.AreEqual(2, countries.Count);
+            Assert.AreEqual("UK", countries[0].Name);
+            Assert.AreEqual("en", countries[0].Symbol);
+            Assert.AreEqual("USA", countries[1].Name);
+            Assert.AreEqual("us", countries[1].Symbol);
         }
 
     }
--- NFlat/ObjectFlattener.cs
+++ NFlat/ObjectFlattener.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Extensions.Primitives;
 using System;
+using System.Buffers.Text;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace NFlat
 {
@@ -91,11 +95,18 @@ namespace NFlat
         }
     }
 
-    public class ObjectFlattener<T, TRawValue> where T : new()
+    public class ObjectFlattener<T, TRawValue> : IEqualityComparer<StringSegment>
+        where T : new()
     {
-        private readonly Dictionary<StringSegment, IPropertyMap<TRawValue>> _propertyMaps = new Dictionary<StringSegment, IPropertyMap<TRawValue>>();
+        private readonly Dictionary<StringSegment, IPropertyMap<TRawValue>> _propertyMaps;
 
-        private readonly Dictionary<StringSegment, IConstructorMap> _constructorMaps = new Dictionary<StringSegment, IConstructorMap>();
+        private readonly Dictionary<StringSegment, IConstructorMap> _constructorMaps;
+
+        public ObjectFlattener()
+        {
+            this._propertyMaps = new Dictionary<StringSegment, IPropertyMap<TRawValue>>(this);
+            this._constructorMaps = new Dictionary<StringSegment, IConstructorMap>(this);
+        }
 
         public ObjectFlattener<T, TRawValue> MapProperty(string path, IPropertyMap<TRawValue> propertyMap)
         {
@@ -115,7 +126,7 @@ namespace NFlat
             object cur = default;
             var idx = 0;
             var processedMaps = new HashSet<StringSegment>();
-            foreach (var p in data.Keys)
+            foreach (var p in data.Keys.OrderBy(q => q))
             {
                 cur = result;
                 var prop = StringSegment.Empty;
@@ -128,8 +139,22 @@ namespace NFlat
                     if (_constructorMaps.TryGetValue(leftSideOfProp, out var constructorPropertyMap))
                     {
                         if (processedMaps.Add(leftSideOfProp))
+                        {
                             constructorPropertyMap.Construct(cur);
-                        cur = constructorPropertyMap.Get(cur);
+                        }
+                        var propAsBytes = MemoryMarshal.AsBytes(prop.AsSpan());
+                        var tempAsBytes = MemoryMarshal.AsBytes(temp.AsSpan());
+                        if (!Utf8Parser.TryParse(propAsBytes, out int _, out var _))
+                        {
+                            cur = constructorPropertyMap.Get(cur);
+                        }
+                        else
+                        {
+                            if (!Utf8Parser.TryParse(tempAsBytes, out int _, out var _))
+                            {
+                                cur = constructorPropertyMap.Get(cur);
+                            }
+                        }
                     }
                     prop = temp;
                     last = idx + 1;
@@ -140,6 +165,51 @@ namespace NFlat
                 }
             }
             return result;
+        }
+
+        bool IEqualityComparer<StringSegment>.Equals(StringSegment obj1, StringSegment obj2)
+        {
+            if (obj1.Length != obj2.Length)
+                return false;
+            var pointerOf1 = 0;
+            var pointerOf2 = 0;
+            while (pointerOf1 < obj1.Length && pointerOf2 < obj2.Length)
+            {
+                var charOfObj1 = obj1[pointerOf1++];
+                var charOfObj2 = obj2[pointerOf2++];
+                if (char.IsNumber(charOfObj1))
+                    charOfObj1 = '*';
+                if (char.IsNumber(charOfObj2))
+                    charOfObj2 = '*';
+                if (!charOfObj1.Equals(charOfObj2))
+                    return false;
+            }
+            return true;
+        }
+
+        int IEqualityComparer<StringSegment>.GetHashCode(StringSegment obj)
+        {
+            if (obj.Length == 0)
+                return StringSegment.Empty.GetHashCode();
+            var index = 0;
+            var charOfObj = obj[index++];
+            if (char.IsNumber(charOfObj))
+                charOfObj = '*';
+            var h = charOfObj.GetHashCode();
+            while (index < obj.Length)
+            {
+                charOfObj = obj[index++];
+                if (char.IsNumber(charOfObj))
+                    charOfObj = '*';
+                h = CombineHashCodes(h, charOfObj.GetHashCode());
+            }
+            return h;
+        }
+
+        //https://github.com/dotnet/corefx/blob/664d98b3dc83a56e1e6454591c585cc6a8e19b78/src/Common/src/CoreLib/System/Tuple.cs
+        public virtual int CombineHashCodes(int h1, int h2)
+        {
+            return (((h1 << 5) + h1) ^ h2);
         }
     }
 }
